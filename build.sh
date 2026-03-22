297494eb575cc965e1275dd28a2351c78d4a0ccc
Amari Cross <amaricross111@gmail.com>
2026-03-21 23:50:04 -0500

    fix: update build.sh to install specific spaCy model version

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 pip install -r requirements.txt
-python -m spacy download en_core_web_sm
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.1/en_core_web_sm-3.7.1-py3-none-any.whl
